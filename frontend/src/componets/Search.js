84aa91c87ec42fca40ace8fd1c264c2f31a0adb2
lamech23 <lamechcruze@gmail.com>
2023-11-23 21:06:03 +0300

    minor update

--- frontend/src/componets/Search.js
+++ frontend/src/componets/Search.js
@@ -10,7 +10,6 @@ function Search() {
   const [search, setSearch] = useState(""); // Store the user's search query.
   const navigate = useNavigate();
   // Function to handle search and navigate based on search results.
-  console.log(results);
   const handleSearch = () => {
     if (results?.length > 0) {
       const house = results;
@@ -58,34 +57,16 @@ function Search() {
       <div className="search-bar hidden lg:flex absolute right-1/3 left-1/3 items-center">
         <form className="w-full max-w-lg">
           <div class="relative">
-            <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
-              <svg
-                class="w-4 h-4 text-gray-500 dark:text-gray-400"
-                aria-hidden="true"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 20 20"
-              >
-                <path
-                  stroke="currentColor"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
-                  d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
-                />
-              </svg>
-            </div>
+            <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none"></div>
             <input
               type="search"
               id="default-search"
-              class="block w-full p-4 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-              placeholder=" Search for Arts, collections, and accounts"
+              class="block w-full p-4 pl-10 text-sm  border border-gray-300 rounded-lg bg-teal-100 focus:ring-blue-500 focus:border-blue-500  dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              placeholder="  Search for house category, price location"
               value={search}
               onChange={handleChange}
               onKeyPress={handleKeyPress}
-              
             />
-            <span className="matterial-symbols-outlined cursor-pointer" onClick={handleSearch}>search</span>
           </div>
           {search && (
             <div className="absolute mt-2 bg-white w-full rounded-md border border-gray-300 shadow-lg">
@@ -97,34 +78,45 @@ function Search() {
                 ) : (
                   search.length >= 2 && (
                     <div>
-                      {Array.isArray(results) &&
-                        results.length > 0 && (
-                          <div>
-                            <span className="block p-2 bg-gray-200">
-                              houses
-                            </span>
-                            {results?.map((result, index) => (
-                              <div
-                                key={index}
-                                className="p-2 border-b border-gray-300 flex items-center"
-                              >
-                                <img
-                                  src={`http://localhost:4000/${result.image}`}
-                                  className="w-12 h-12 rounded-full"
-                                  alt=""
-                                />
-                                <div className="ml-2 text-lg">
-                                  <Link
-                                    to={`/MoreDetails/${result.id}`}
-                                    className=""
-                                  >
-                                    {result.title}
-                                  </Link>
-                                </div>
+                      {Array.isArray(results) && results.length > 0 && (
+                        <div>
+                          <span className="block p-2 bg-gray-200">houses</span>
+                          {results?.map((result, index) => (
+                            <div
+                              key={index}
+                              className="p-2 border-b border-gray-300 flex items-center"
+                            >        <svg
+                            class="w-4 h-4 text-gray-500 dark:text-gray-400"
+                            aria-hidden="true"
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="none"
+                            viewBox="0 0 20 20"
+                          >
+                            <path
+                              stroke="currentColor"
+                              stroke-linecap="round"
+                              stroke-linejoin="round"
+                              stroke-width="2"
+                              d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
+                            />
+                          </svg>
+                              <img
+                                src={`http://localhost:4000/${result.image}`}
+                                className="w-12 h-12 rounded-full"
+                                alt=""
+                              />
+                              <div className="ml-2 text-lg">
+                                <Link
+                                  to={`/MoreDetails/${result.id}`}
+                                  className=""
+                                >
+                                  {result.title}
+                                </Link>
                               </div>
-                            ))}
-                          </div>
-                        )}
+                            </div>
+                          ))}
+                        </div>
+                      )}
                     </div>
                   )
                 )}

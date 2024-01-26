f6f95d7bb599f635f74da427f1cd339901ccd610
muga20 <murimibrian18@outlook.com>
2024-01-27 02:25:18 +0300

    Updates

--- backend/Routes/imageRoute.js
+++ backend/Routes/imageRoute.js
@@ -3,15 +3,11 @@ const router =express.Router()
 const {
     createImages,
     getImages
-}=require('../controllers/imageController')
-const {requireAuth } =require('../middlleware/requireAuth')
-const {imageUpload } =require('../middlleware/upload')
+}=require('../controllers/imageController');
+const {requireAuth } =require('../middlleware/requireAuth');
+const {imageUpload } =require('../middlleware/upload');
 
-
-router.post('/', requireAuth, imageUpload, createImages)
-router.get('/fetchImages',requireAuth, getImages)
-
-
-
+router.post('/', requireAuth, imageUpload, createImages);
+router.get('/fetchImages',requireAuth, getImages);
 
 module.exports = router
